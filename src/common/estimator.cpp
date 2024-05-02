f7d3d986cca7cec68fdcbc69f31bb68ca5e27b6e
zybz-ITX-Ubuntu20.04 <1137488143@qq.com>
2024-05-02 18:50:45 +0800

    状态估计窗口滑动为0.1阈值，速度连续

--- src/common/estimator.cpp
+++ src/common/estimator.cpp
@@ -95,7 +95,7 @@ void Estimator::step(const std::shared_ptr<Gait> &gait, const std::shared_ptr<Ro
             _R.block<3, 3>(12 + 3 * i, 12 + 3 * i) = _large_variance * _I3; // 摆动腿速度测量 大噪声
             _R(24 + i, 24 + i) = _large_variance;                           // 摆动腿高度测量 大噪声
         } else {
-            _trust = windowFunc(gait->getPhase(i), 0.2);
+            _trust = windowFunc(gait->getPhase(i), 0.1);
             // 摆动腿位置估计噪声随触地相位增大而变小
             _Q.block<3, 3>(6 + 3 * i, 6 + 3 * i) =
                     (1 + (1 - _trust) * _large_variance) * _Q_init.block<3, 3>(6 + 3 * i, 6 + 3 * i);
