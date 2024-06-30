d03e423613a0111fa3cf7449b33183aa3644c6ca
zybz-ITX-Ubuntu20.04 <1137488143@qq.com>
2024-06-30 14:24:04 +0800

    ✨ feat: 标定新腿趴着时角度

--- include/FSM/state_fixed_down.hpp
+++ include/FSM/state_fixed_down.hpp
@@ -5,12 +5,11 @@
 #ifndef BUPT_DOG_CONTROLLER2_STATE_FIXED_DOWN_HPP
 #define BUPT_DOG_CONTROLLER2_STATE_FIXED_DOWN_HPP
 
-
 #include "fsm_state.hpp"
 
 class State_FixedDown : public FSMState {
-public:
-    explicit State_FixedDown(const std::shared_ptr<CtrlComponents> &ctrl_comp);
+  public:
+    explicit State_FixedDown(const std::shared_ptr<CtrlComponents>& ctrl_comp);
 
     void enter() override;
 
@@ -20,14 +19,15 @@ public:
 
     FSMStateName checkChange() override;
 
-private:
-    const double _target_pos[12] = {0.2558652683423687, 1.21265, -2.77594, -0.2558652683423687, 1.21265, -2.77594,
-                                    0.2558652683423687, 1.21265, -2.77594, -0.2558652683423687, 1.21265, -2.77594};
+  private:
+    const double _target_pos[12] = {19.68 / 180.0 * M_PI, 61.959 / 180.0 * M_PI, -151.641 / 180.0 * M_PI,
+                                    -19.68 / 180.0 * M_PI, 61.959 / 180.0 * M_PI, -151.641 / 180.0 * M_PI,
+                                    19.68 / 180.0 * M_PI, 61.959 / 180.0 * M_PI, -151.641 / 180.0 * M_PI,
+                                    -19.68 / 180.0 * M_PI, 61.959 / 180.0 * M_PI, -151.641 / 180.0 * M_PI};
     Vec12 _start_pos;
     double _percent = 0;
     double _duration = 2; // 单位 s ，完成站立的周期
     double _freq;         // 状态机执行频率
 };
-
 
 #endif //BUPT_DOG_CONTROLLER2_STATE_FIXED_DOWN_HPP
